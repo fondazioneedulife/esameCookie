201c6a6d100daec4c847068565ffa21aabb46a1c
giulymezz <ggiulia.mmazzi@gmail.com>
2024-12-20 12:37:08 +0100

    task1

--- frontend/src/routes/Extract.tsx
+++ frontend/src/routes/Extract.tsx
@@ -1,31 +1,49 @@
 import { Box, Stack, Typography } from "@mui/joy";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { User } from "../../../api";
-import { TaskBox } from "../components/TaskBox";
+// import { TaskBox } from "../components/TaskBox";
 import { useCurrentUser } from "../lib/useCurrentUser";
-// import { config } from "../config";
-// import { useFetch } from "../lib/useFetch";
+import { useFetch } from "../lib/useFetch";
+import { config } from "../config";
 
 // TODO Task 1 - implementa la logica che manca: estrai il destinatario (chiamando una api) e visualizza il risultato
 
 export const Extract: React.FC = () => {
   const currentUser = useCurrentUser();
-  const [recipient] = useState<User | null>();
-  const [error] = useState();
+  const [recipient, setRecipient] = useState<User | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  
+  const fetch = useFetch();
 
-  // const fetch = useFetch();
+  useEffect(() => {
+    const fetchRecipient = async () => {
+      try {
+        const response = await fetch(${config.API_BASEPATH}/api/extract, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(currentUser),
+        });
+        const user: User = await response.json();
+        setRecipient(user);
+      } catch (err) {
+        console.error("Errore nella richiesta API:", err);
+        setError("Errore nella richiesta API");
+      }
+    };
+    if (currentUser) {
+      fetchRecipient();
+    }
+  }, []);
+
+  
 
   if (error) {
     return "Mi dispiace, tutti i destinatari sono stati già estratti";
   }
 
   if (!recipient) {
-    return (
-      <TaskBox>
-        Mmmmhh.... mi sa che manca la funzione per estrarre il destinatario,
-        scrivila tu!
-      </TaskBox>
-    ); // quando hai finito, togli questa riga e usa la seguente
     return "Attendi mentre estraggo il destinatario....";
   }
 
